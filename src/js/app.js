1783a7fd661dd1bba82625c72b1c017d0d76340e
Melina Cirverius <mc222ap@student.lnu.se>
2018-12-05 13:25:39 +0100

    Testing out input box for answers

--- src/js/app.js
+++ src/js/app.js
@@ -19,3 +19,30 @@ req.addEventListener('load', function () {
 
 req.open('GET', 'http://vhost3.lnu.se:20080/question/1')
 req.send()
+
+let theAnswer
+
+function createAnswerBox () {
+  theAnswer = document.createElement('input')
+  theAnswer.setAttribute('type', 'text')
+  theAnswer.setAttribute('id', 'answerBox')
+  document.querySelector('#quiz').appendChild(theAnswer)
+}
+createAnswerBox()
+
+function getAnswer () {
+  let answOne = document.querySelectorAll('#answerBox input')[0]
+  console.log(answOne)
+
+  document.querySelector('#answerBox').addEventListener('blur', event => {
+    if (answOne !== undefined) {
+      console.log(answOne.toString())
+    }
+  }, true)
+}
+
+getAnswer()
+
+let answ = new window.XMLHttpRequest()
+
+answ.addEventListener()
